c368f58480e431a06726914bf8dd329366582dfd
Daan <daan@tutanota.de>
2018-03-03 15:16:24 +0100

    Scaffolding created

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+// Font Awesome initialize
+import fontawesome from '@fortawesome/fontawesome';
+import solid from '@fortawesome/fontawesome-free-solid';
+import regular from '@fortawesome/fontawesome-free-regular';
+import brands from '@fortawesome/fontawesome-free-brands';
